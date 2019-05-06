e074edd10d658aa8f56e61dc6a73790367d4c547
Jesse Silva <thebestjesse76@gmail.com>
2019-05-06 13:21:48 -0600

    updating ddl and mysql

--- capstone-lunch.sql
+++ capstone-lunch.sql
@@ -1,9 +1,14 @@
+drop table if exists favorite;
+drop table if exists pictures;
+drop table if exists profile;
+drop table if exists restaurant;
+
 create table profile (
     profileId        binary(16)   not null,
-    profileEmail     varchar(126) not null,
-    profileFirstName varchar(126) not null,
-    profileLastName  varchar(126) not null,
-    profileHash      varchar(126) not null,
+    profileEmail     varchar(128) not null,
+    profileFirstName varchar(128) not null,
+    profileLastName  varchar(128) not null,
+    profileHash      char(97) not null,
     unique (profileEmail),
     primary key (profileId)
 );
@@ -19,20 +24,22 @@ create table restaurant (
     restaurantThumbnail varchar(255) not null,
     primary key (restaurantId)
 );
-
-create table favorite (
-    favoriteProfileId binary (16),
-    favoriteRestaurantId binary (16),
-    foreign key (favoriteProfileId) references profile(profileId),
-    foreign key (favoriteRestaurantId) references restaurant(restaurantId)
-
-
-);
-
 create table pictures (
     pictureId binary (16) not null,
     pictureRestaurantId varchar(255) not null,
     pictureUrl varchar(128)not null,
     foreign key (pictureRestaurantId) references restaurant(restaurantId),
     primary key (pictureId)
+
+
 );
+
+create table favorite (
+    favoriteProfileId binary (16),
+    favoriteRestaurantId binary (16),
+    foreign key (favoriteProfileId) references profile(profileId),
+    foreign key (favoriteRestaurantId) references restaurant(restaurantId)
+);
+
+
+
