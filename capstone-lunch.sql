b3af860fb37f361eea784d93776e34490cae4522
Jesse Silva <thebestjesse76@gmail.com>
2019-05-06 16:44:16 -0600

    fixed it and now running

--- capstone-lunch.sql
+++ capstone-lunch.sql
@@ -1,5 +1,5 @@
-drop table if exists pictures;
 drop table if exists favorite;
+drop table if exists pictures;
 drop table if exists restaurant;
 drop table if exists profile;
 
@@ -17,12 +17,20 @@ create table restaurant (
     restaurantId binary (16) not null,
     restaurantAddress varchar(128) not null,
     restaurantName varchar(128) not null,
-    restaurantLng decimal (10) not null,
-    restaurantLat decimal (10) not null,
+    restaurantLng decimal (9,6) not null,
+    restaurantLat decimal (9,6) not null,
     restaurantPrice numeric (32) not null,
     restaurantReviewRating integer (5) not null,
     restaurantThumbnail varchar(255) not null,
     primary key (restaurantId)
+);
+
+create table pictures (
+    pictureId binary (16) not null,
+    pictureRestaurantId binary (16) not null,
+    pictureUrl varchar(255)not null,
+    foreign key (pictureRestaurantId) references restaurant(restaurantId),
+    primary key (pictureId)
 );
 
 create table favorite (
@@ -30,14 +38,4 @@ create table favorite (
     favoriteRestaurantId binary (16),
     foreign key (favoriteProfileId) references profile(profileId),
     foreign key (favoriteRestaurantId) references restaurant(restaurantId)
-
-
-);
-
-create table pictures (
-    pictureId binary (16) not null,
-    pictureRestaurantId varchar(255) not null,
-    pictureUrl varchar(255)not null,
-    foreign key (pictureRestaurantId) references restaurant(restaurantId),
-    primary key (pictureId)
 );
