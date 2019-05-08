af99d3e9db553474132d1f1c39c3d1d972acc5a7
Jesse Silva <thebestjesse76@gmail.com>
2019-05-08 11:17:53 -0600

    did autoload validate and validate uuid

---
+++ php/Classes/ValidatedateTrait.php
@@ -0,0 +1,8 @@
+<?php
+namespace jsilva85\capstonelunch;
+
+/**
+ *Trait Validate a mySQL Date
+ *
+ *This trait will inject a private method to validate a mySQL style date (e.g., 2016-01-15 15:32:48.643216)
+ */
