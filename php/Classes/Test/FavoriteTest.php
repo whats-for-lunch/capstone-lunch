43bc1c4f7349f3e55be504b344d54589d73d3a62
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-05-17 22:18:41 -0700

    Began work on the public final function set up for the Favorite class.

--- php/Classes/Test/FavoriteTest.php
+++ php/Classes/Test/FavoriteTest.php
@@ -18,11 +18,33 @@ require_once(dirname(__DIR__) . "/autoload.php");
  */
 
 class FavoriteTest extends {
+
 	/**
 	 * Profile that created the Favorite; this is for foreign key relations
 	 * @var Profile Profile
 	 */
 	protected $profile = null;
 
-	
+	/**
+	 * Restaurant that created the Favorite; this is for foreign key relations
+	 * @var string $restaurant
+	 */
+	protected $restaurant = null;
+
+	/**
+	 * create dependent objects before running each test
+	 */
+	public final function setUp() : void {
+		//run the default setUp() method first
+		parent::setUp();
+		$password = "abc123";
+		$this->VALID_PROFILE_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+		$this->VALID_ACTIVATION_TOKEN = bin2hex(random_bytes(16));
+
+		//create and insert a Profile to the test Favorite
+		$profileId = generateUuidV4();
+		$this->profile = new Profile($profileId, $this->VALID_ACTIVATION_TOKEN, )
+	}
+
+
 }
