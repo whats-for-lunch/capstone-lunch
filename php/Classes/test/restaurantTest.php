a968f1463b7248bf7748adf889469ddf2c16abcc
Jamie Amparan <jleespooks@gmail.com>
2019-05-18 22:43:07 -0600

    setting up unit testing

--- php/Classes/test/restaurantTest.php
+++ php/Classes/test/restaurantTest.php
@@ -2,8 +2,27 @@
 
 namespace whatsForLunch\capstone\test;
 
-use whatsForLunch\capstone\favorites;
-use whatsForLunch\capstone\profile;
+use whatsForLunch\capstone\favorite;
+use whatsForLunch\capstone\picture;
 use whatsForLunch\capstone\restaurant;
 
-require_once ("")
+require_once ("whatsForLunchTesting.php");
+
+// get the autoloader
+require_once (dirname(__DIR__). "/autoloader.php");
+
+// get the uuid generator
+require_once (dirname(__DIR__,2). "/lib/uuid.php");
+
+/**
+ * Full PHPunit test of the Restaurant class
+ *
+ *@see restaurant
+ *@author whatsForLunch capstone
+ */
+
+class restaurantTest extends whatsForLunchTesting {
+	/**
+	 * 
+	 */
+}
