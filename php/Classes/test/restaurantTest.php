a9376a5e1e24223220cbc7c3d40ba2f2b68a9803
Jamie Amparan <jleespooks@gmail.com>
2019-05-18 22:30:48 -0600

    starting unit testing

---
+++ php/Classes/test/restaurantTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace whatsForLunch\capstone\test;
+
+use whatsForLunch\capstone\favorites;
+use whatsForLunch\capstone\profile;
+use whatsForLunch\capstone\restaurant;
+
+require_once ("")
