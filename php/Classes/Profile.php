93f61f89635eac7af928b6776b5b87cebfa2d4b9
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-05-20 08:59:02 -0700

    Added jsonSerialize function in profile class.

--- php/Classes/Profile.php
+++ php/Classes/Profile.php
@@ -419,6 +419,16 @@ class Profile implements \JsonSerializable
 		return($profileActivationToken);
 	}
 
+	/**
+	 * formats the state variables for JSON serialization
+	 *
+	 * @return array resulting state variables to serialize
+	 **/
+	public function jsonSerialize(): array {
+		$fields = get_object_vars($this);
+		return ($fields);
+	}
+
 
 
 }
