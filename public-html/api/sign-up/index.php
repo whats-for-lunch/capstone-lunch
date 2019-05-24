0af6da4bcb262931b4511c7b82732e7bef3f547f
Jesse Silva <thebestjesse76@gmail.com>
2019-05-24 11:27:47 -0600

    updated api with reply message and verified session

--- public-html/api/sign-up/index.php
+++ public-html/api/sign-up/index.php
@@ -5,4 +5,23 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once ("/etc/apache2/capstone-mysql/Secrets.hp");
 
-use
+use WhatsForLunch\CapstoneLunch\{
+  //not testing any class not putting anything specific
+};
+
+/**
+ * api for the sign up
+ *
+ * @author {} thebestjesse76@gmail.com
+ *
+ **/
+
+//verify the session, start if not the new session
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
