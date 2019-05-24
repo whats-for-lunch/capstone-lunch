e8d00e819da597c0bfbe672358af6913cd414187
Jesse Silva <thebestjesse76@gmail.com>
2019-05-24 11:12:40 -0600

    starting working on api for sign up created directory for signup and conitnued with require once

---
+++ public-html/api/sign-up/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once ("/etc/apache2/capstone-mysql/Secrets.hp");
+
+use
