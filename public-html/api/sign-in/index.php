9a696f9bcba19147d95b185499dd13285c29d65f
Jamie Amparan <jleespooks@gmail.com>
2019-05-27 12:00:25 -0600

    starting restaurant api

--- public-html/api/sign-in/index.php
+++ public-html/api/sign-in/index.php
@@ -4,13 +4,15 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
-use DeepDiveDatingApp\DeepDiveDating\User;
+
+use WhatsForLunch\CapstoneLunch\Profile;
+
 /**
- *  API for app sign in, User class
+ *  API for app sign in, Profile class
  *
  * POST requests are supported.
  *
- * @author Nehomah Mora <nmora9@cnm.edu>
+ * @author Jamie Amparan <jamparan3@cnm.edu>
  **/
 //Verify the session. If it's not active, start it.
 if(session_status() !== PHP_SESSION_ACTIVE) {
@@ -22,16 +24,16 @@ $reply->status = 200;
 $reply->data = null;
 try {
 	// Grab the mySQL connection
-	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort23/dateadan.ini");
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort24/whatsforlunch");
 	$pdo = $secrets->getPdoObject();
 	// Determine which HTTP method was used.
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	// sanitize inputs
-	//$userId = filter_input(INPUT_GET, "userId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	//$userEmail = filter_input(INPUT_GET, "userEmail", FILTER_SANITIZE_EMAIL);
-	//$userHash = filter_input(INPUT_GET, "userHash", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	//make sure the userId is valid for methods that require it
-	//if(($method === "DELETE" || $method === "PUT") && (empty($userId) === true)) {
+	//$profiled = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	//$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_EMAIL);
+	//$profileHash = filter_input(INPUT_GET, "profileHash", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	//make sure the profileId is valid for methods that require it
+	//if(($method === "DELETE" || $method === "PUT") && (empty($profileId) === true)) {
 	//throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	//}
 	//}
@@ -43,49 +45,49 @@ try {
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 		// Check for the email (required field)
-		if(empty($requestObject->userEmail) === true) {
+		if(empty($requestObject->profileEmail) === true) {
 			throw (new \InvalidArgumentException("An email address must be entered.", 401));
 		} else {
-			$userEmail = filter_var($requestObject->userEmail, FILTER_SANITIZE_EMAIL);
+			$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
 		}
 		// Check for the password (required field).
-		if(empty($requestObject->userPassword) === true) {
+		if(empty($requestObject->profileActivationToken) === true) {
 			throw (new \InvalidArgumentException("A password must be entered.", 401));
 		} else {
-			$userPassword = $requestObject->userPassword;
+			$profileActivationToken = $requestObject->profileActivationToken;
 		}
-		// Grab the user from the database by the email address provided.
-		$user = User::getUserByEmail($pdo, $userEmail);
-		if(empty($user) === true) {
+		// Grab the profile from the database by the email address provided.
+		$profile = profile::getProfileByProfileEmail($pdo, $profileEmail);
+		if(empty($profile) === true) {
 			throw(new \InvalidArgumentException("Invalid Email", 401));
 		}
-		$user->setUserActivationToken(null);
-		$user->update($pdo);
+		$profile->setProfileActivationToken(null);
+		$profile->update($pdo);
 		// If the profile activation is not null throw an error
-		if($user->getUserActivationToken() !== null) {
-			throw (new \InvalidArgumentException("You are not allowed to sign in unless you have activated your account", 403));
+		if($profile->getProfileActivationToken() !== null) {
+			throw (new \InvalidArgumentException("Log In To Save Your Favorites!", 403));
 		}
 		// Verify hash is correct
-		if(password_verify($requestObject->userPassword, $user->getUserHash()) === false) {
-			throw(new \InvalidArgumentException("Invalid password.", 401));
+		if(password_verify($requestObject->profileActivationToken, $profile->getProfileHash()) === false) {
+			throw(new \InvalidArgumentException("Invalid Token.", 401));
 		}
-		// Grab the user from the database and put it into a session.
-		$user = User::getUserByUserId($pdo, $user->getUserId());
-		$_SESSION["user"] = $user;
+		// Grab the profile from the database and put it into a session.
+		$profile = profile::getProfileByProfileActivationToken($pdo, $profile->getProfileId());
+		$_SESSION["profile"] = $profile;
 		// Create the authorization payload
 		$authObject = (object)[
-			"userId" => $user->getUserId(),
-			"userHandle" => $user->getUserHandle()
+			"profileId" => $profile->getProfileId(),
+			"profileActivationToken" => $profile->getProfileActivationToken()
 		];
 		// Create and set the JWT
 		setJwtAndAuthHeader("auth", $authObject);
-		$reply->message = $authObject->userId;
+		$reply->message = $authObject->profileId;
 	} else {
 		throw (new \InvalidArgumentException("Invalid HTTP request!"));
 	}
-}catch(\Exception | \TypeError $exception) {
+} catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
-	$reply->mesage = $exception->getMessage();
+	$reply->message = $exception->getMessage();
 }
 // Sets up the response header.
 header("Content-type: application/json");
