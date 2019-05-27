a5cd00ba23a8bf37d7772bd4dc371e542d227a09
Jamie Amparan <jleespooks@gmail.com>
2019-05-27 14:12:39 -0600

    adding verify session to restaurant api

--- public-html/api/restaurant/index.php
+++ public-html/api/restaurant/index.php
@@ -4,4 +4,14 @@ require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
-use WhatsForLunch\Capstone-Lunch\Restaurant;
+
+use WhatsForLunch\CapstoneLunch\Restaurant;
+
+/**
+ * api for the foodTruck class
+ *
+ **/
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
