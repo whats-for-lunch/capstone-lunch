84f40ddca44c294439ed0792774abaaaf77025b0
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-05-29 10:49:23 -0700

    Updated namespace to match directories to get it to work.

--- php/public-html/api/activation/index.php
+++ php/public-html/api/activation/index.php
@@ -1,7 +1,8 @@
 <?php
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
-require_once dirname("/etc/apache2/capstone-mysql/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/Secrets.php");
 
 use WhatsForLunch\CapstoneLunch\Profile;
 /**
