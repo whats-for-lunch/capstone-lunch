b96cb09cc0eab7ee3f8bc4ac4ad6f2f865c23199
Jesse Silva <thebestjesse76@gmail.com>
2019-06-03 09:45:30 -0600

    made a new directory for pictur-api set up upstream to develop and intiated start with picture-api

---
+++ php/public-html/api/picture-api/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once (dirname(__DIR__, 3) . "/php/Classes/autoload.php");
+require_once (dirname(__DIR__, 3) . "/php/lib/jwt.php");
+require_once (dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once ("/etc/apache2/captstone-mysql/Secrets.php");
+
+
+use WhatsForLunch\CapstoneLunch\ {Picture
+};
