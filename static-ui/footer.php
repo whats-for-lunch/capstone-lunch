2a33018ada0025b1a8d2982fa39c2cc62cbf1585
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-06-03 20:43:28 -0700

    Created static ui files for footer and about us text.

---
+++ static-ui/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+				integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+		<!-- CSS Stylesheet -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<title>
+			Footer for Capstone
+		</title>
+	</head>
+	<body>
+		<footer>
+			<div class="container" id="footer">
+				<a href="https://github.com/JeffreyGallegosCoding" class="github">
+					<i class="fab fa-github fa-2x"></i>
+			</div>
+		</footer>
+	</body>
+</html>
+
