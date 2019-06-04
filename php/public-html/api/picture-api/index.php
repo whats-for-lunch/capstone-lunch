24a9ef217027b92c52cd365894c9038ded9f9994
Jesse Silva <thebestjesse76@gmail.com>
2019-06-04 14:29:02 -0600

    made some corrections changes

--- php/public-html/api/picture-api/index.php
+++ php/public-html/api/picture-api/index.php
@@ -7,7 +7,9 @@ require_once(dirname(__DIR__, 3) . "/lib/uuid.php");
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 
-use WhatsForLunch\CapstoneLunch\{Picture};
+use WhatsForLunch\CapstoneLunch\ {
+    Picture
+};
 
 
 /**
@@ -28,10 +30,10 @@ $reply->data = null;
 
 try {
     //grab the MySql connection
-    $secrets = new \Secrets("/etc/apache2/capstone-mysql/WhatsForLunch.ini");
-    $pdo = $secrets->getPDOObject();
+    $secrets = new \Secrets("/etc/apache2/capstone-mysql/whatsforlunch.ini");
+    $pdo = $secrets->getPdoObject();
     //waiting on yelp data not sure if this would be how to use
-//    $yelp = $secrets->getSecret("yelp");
+    // $yelp = $secrets->getSecret("yelp");
 
     //determine which HTTP method was used
     $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
@@ -40,18 +42,11 @@ try {
 //        setXsrfCookie();
 
 //Sanitizing all inputs
-        $id = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-        $profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-        $profileFirstName = filter_input(INPUT_GET, "profileFirstName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-        $profileLastName = filter_input(INPUT_GET, "profileLastName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+        $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
         $restaurantId = filter_input(INPUT_GET, "restaurantId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
         $restaurantThumbnail = filter_input(INPUT_GET, "restaurantThumbnail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
         $favoriteProfileId = filter_input(INPUT_GET, "favoriteProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-        //have to make sure that id is valid for all methods that require it
-        if (($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
-            throw(new \InvalidArgumentException("id cannot be empty of negative", 405));
-        }
 
         if ($method == "GET") {
             // set XSRF-TOKEN to prev
