66a9786eb6946286b5c855360ab869d3d67d7399
Jesse Silva <thebestjesse76@gmail.com>
2019-06-04 13:47:20 -0600

    updated images made them more rounded. Added header to site to identify what they are. inputed info from random restuarants in area

--- favorite-staticui/favorite-staticui.php
+++ favorite-staticui/favorite-staticui.php
@@ -15,33 +15,50 @@
 
 
 
-
-
+<div class="row justify-content-center">
+<h1><strong>Favorites</strong></h1>
+</div>
 <section id="card decks">
-<div class="container">
+<div class="container my-8 border-warning">
     <div class="card-deck">
+
         <div class="card">
-            <img class="card-img-top" src="https://s3-media2.fl.yelpcdn.com/bphoto/UEnfWw0AdsVhMzmvLswgzg/o.jpg" alt="Card image cap">
+            <img class="card-img-top" src="https://s3-media2.fl.yelpcdn.com/bphoto/UEnfWw0AdsVhMzmvLswgzg/o.jpg" alt="Card image cap" class="rounded">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">Artichoke Cafe</h5>
+                <p class="card-text">American (New), Cafes</p>
+                <p><strong>424 Central Ave SE
+                        Albuquerque, NM 87102</strong></p>
+            </div>
+
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="https://s3-media3.fl.yelpcdn.com/bphoto/-odb7y2_GxASimUqXtNn4Q/o.jpg" alt="Card image cap" class="rounded">
+            <div class="card-body">
+                <h5 class="card-title">Cocina Azul</h5>
+                <p class="card-text">New Mexican Cuisine </p>
+                <p><strong>1134 Mountain Rd NW
+                        Albuquerque, NM 87102</strong></p>
             </div>
         </div>
         <div class="card">
-            <img class="card-img-top" src="https://s3-media3.fl.yelpcdn.com/bphoto/-odb7y2_GxASimUqXtNn4Q/o.jpg" alt="Card image cap">
+            <img class="card-img-top" src="https://s3-media3.fl.yelpcdn.com/bphoto/-odb7y2_GxASimUqXtNn4Q/o.jpg" alt="Card image cap" class="rounded">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">The Grove Cafe & Market</h5>
+                <p class="card-text">Coffee & Tea, American (New), Cafes </p>
+                <p><strong>600 Central Ave SE
+                        Ste A
+                        Albuquerque, NM 87102</strong></p>
             </div>
         </div>
         <div class="card">
-            <img class="card-img-top" src="https://s3-media2.fl.yelpcdn.com/bphoto/1oZOjekcyyA11jsWZ1pGtQ/o.jpg" alt="Card image cap">
+            <img class="card-img-top" src="https://s3-media2.fl.yelpcdn.com/bphoto/UEnfWw0AdsVhMzmvLswgzg/o.jpg" alt="Card image cap" class="rounded">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">Sophia’s</h5>
+                <p class="card-text">American (New), Breakfast & Brunch, Latin American</p>
+                <p><strong> Park Ave SW
+                    Ste 102
+                    Albuquerque, NM 87102</strong></p>
             </div>
         </div>
     </div>
