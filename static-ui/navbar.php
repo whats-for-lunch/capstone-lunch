a70c757ac3044c74b58f64c5b81119dfec54cd1c
Jamie Amparan <jleespooks@gmail.com>
2019-06-04 15:06:01 -0600

    adding require once to head utils

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<?php require_once ("head-utils.php"); ?>
 
-		<title>Title</title>
-	</head>
-	<body>
-			<!--Navbar beginning -->
+
+	<head>
+		<!--Navbar beginning -->
 		<nav class="navbar navbar-dark bg-dark fluid d-flex justify-content-end">
 			<form class="form-inline">
 				<button class="btn btn-outline-warning" type="button">Sign Up</button>
@@ -31,6 +25,5 @@
 		</div>
 			</form>
 		</nav>
-
-	</body>
-</html>
+		<title>Title</title>
+		</head>
