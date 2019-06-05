0ae53e7fb4169e691f11fe6ad9376de00c273d0f
Jamie Amparan <jleespooks@gmail.com>
2019-06-05 11:34:32 -0600

    added a bit to frontpage of static ui

--- static-ui/header.php
+++ static-ui/header.php
@@ -1,9 +1,11 @@
 <?php require_once("head-utils.php"); ?>
 <?php require_once("navbar.php"); ?>
+<?php require_once ("header.php"); ?>
+<?php require_once ("recommended.php"); ?>
 
 
 <head>
-	<div class="jumbotron jumbotron-fluid bg-dark text-warning justify-content-center py-2 ">
+	<div class="jumbotron jumbotron-fluid bg-dark text-warning justify-content-center py-0 ">
 		<div class="container">
 			<div class="row justify-content-center">
 				<h1 class="display-4">WHAT'S FOR LUNCH?</h1>
@@ -15,47 +17,11 @@
 
 	<!-- dummy pic to see full page while building -->
 	<div class="container"
-	<div class="container justify-content-center bg-dark ">
+	<div class="container justify-content-center bg-dark">
 		<img src="img/dummymap.png" height="600px">
 		<div class="column bg-dark">
 		</div>
 	</div>
 
-<!-- our recommended pages + links to our handles -->
-	<div class="card-deck">
-		<div class="card">
-			<img src="..." class="card-img-top" alt="...">
-			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
-			</div>
-			<div class="card-footer">
-				<small class="text-muted">Last updated 3 mins ago</small>
-			</div>
-		</div>
-
-		<div class="card">
-			<img class="img-fluid my-3 rounded-circle " src="img/jamieprop.jpg" class="card-img-top" alt="prop">
-			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
-				<p class="card-text"><a href="https://www.linkedin.com/in/jamie-amparan/" target="_blank">LinkedIn</a>
-					or
-					<a href="https://github.com/jleespooks/" target="_blank">Github</a>"</p>
-			</div>
-			<div class="card-footer">
-				<small class="text-muted">Last updated 3 mins ago</small>
-			</div>
-		</div>
-
-		<div class="card">
-			<img class="img-fluid my-3 md-rounded-circle" src="img/jesseprop.png" class="card-img-top" alt="prop">
-			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
-				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-			</div>
-			<div class="card-footer">
-				<small class="text-muted">Last updated 3 mins ago</small>
-			</div>
-		</div>
-	</div>
 
 </head>
--- static-ui/recommended.php
+++ static-ui/recommended.php
@@ -1,36 +1,58 @@
 <?php require_once("head-utils.php"); ?>
 <?php require_once("navbar.php"); ?>
+<?php require_once ("header.php"); ?>
+<?php require_once ("background.php"); ?>
 
+<!-- our recommended pages + links to our handles -->
 
-<div class="card-deck">
-	<div class="card">
-		<img src="..." class="card-img-top" alt="...">
-		<div class="card-body">
-			<h5 class="card-title">Card title</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-		</div>
-		<div class="card-footer">
-			<small class="text-muted">Last updated 3 mins ago</small>
+<body>
+<div class="jumbotron jumbotron-fluid bg-dark text-warning justify-content-center py-0 ">
+	<div class="container">
+		<div class="row justify-content-center">
+			<h1 class="display-4">Recommended</h1>
+			<p class="lead"></p>
 		</div>
 	</div>
-	<div class="card">
-		<img src="..." class="card-img-top" alt="...">
+</div>
+
+<div class="card-deck bg-dark">
+
+	<div class="card bg-secondary">
+		<img class="img-fluid rounded " src="https://www.fillmurray.com/g/300/300" alt="prop">
 		<div class="card-body">
 			<h5 class="card-title">Card title</h5>
-			<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
 		</div>
 		<div class="card-footer">
-			<small class="text-muted">Last updated 3 mins ago</small>
+			<small class="text-muted"><p class="card-text"><a href="https://www.linkedin.com/in/jamie-amparan/" target="_blank">LinkedIn</a>
+					or
+					<a href="https://github.com/jleespooks/" target="_blank">Github</a></p></small>
 		</div>
 	</div>
-	<div class="card">
-		<img src="..." class="card-img-top" alt="...">
+
+	<div class="card bg-warning">
+		<img class="img-fluid rounded " src="https://www.fillmurray.com/g/300/300"  alt="prop">
 		<div class="card-body">
 			<h5 class="card-title">Card title</h5>
 			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
 		</div>
 		<div class="card-footer">
-			<small class="text-muted">Last updated 3 mins ago</small>
+			<small class="text-muted"><p class="card-text"><a href="https://www.linkedin.com/in/jamie-amparan/" target="_blank">LinkedIn</a>
+					or
+					<a href="https://github.com/jleespooks/" target="_blank">Github</a></p></small>
+		</div>
+	</div>
+
+	<div class="card bg-dark">
+		<img class="img-fluid  rounded" src="https://www.fillmurray.com/g/300/300" alt="prop">
+		<div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+		</div>
+		<div class="card-footer">
+			<small class="text-muted"><p class="card-text"><a href="https://www.linkedin.com/in/jamie-amparan/" target="_blank">LinkedIn</a>
+					or
+					<a href="https://github.com/jleespooks/" target="_blank">Github</a></p></small>
 		</div>
 	</div>
 </div>
+</body>
