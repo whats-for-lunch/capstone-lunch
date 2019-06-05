d9b00120e337b9dfc827165b67eb87ad31ffd335
Jamie Amparan <jleespooks@gmail.com>
2019-06-04 22:21:43 -0600

    playing with bootstrap images still

--- static-ui/header.php
+++ static-ui/header.php
@@ -1,12 +1,61 @@
-<?php require_once ("head-utils.php"); ?>
-<?php require_once ("navbar.php"); ?>
+<?php require_once("head-utils.php"); ?>
+<?php require_once("navbar.php"); ?>
 
-<div class="jumbotron jumbotron-fluid bg-dark text-warning justify-content-center py-2 ">
-	<div class="container">
-		<div class= "row justify-content-center" >
-		<h1 class="display-4">WHAT'S FOR LUNCH?</h1>
-			<p class="lead"></p>
+
+<head>
+	<div class="jumbotron jumbotron-fluid bg-dark text-warning justify-content-center py-2 ">
+		<div class="container">
+			<div class="row justify-content-center">
+				<h1 class="display-4">WHAT'S FOR LUNCH?</h1>
+				<p class="lead"></p>
+			</div>
+		</div>
 	</div>
-</div>
-</div>
 
+
+	<!-- dummy pic to see full page while building -->
+	<div class="container"
+	<div class="container justify-content-center bg-dark ">
+		<img src="img/dummymap.png" height="600px">
+		<div class="column bg-dark">
+		</div>
+	</div>
+
+<!-- our recommended pages + links to our handles -->
+	<div class="card-deck">
+		<div class="card">
+			<img src="..." class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">Card title</h5>
+			</div>
+			<div class="card-footer">
+				<small class="text-muted">Last updated 3 mins ago</small>
+			</div>
+		</div>
+
+		<div class="card">
+			<img class="img-fluid my-3 rounded-circle " src="img/jamieprop.jpg" class="card-img-top" alt="prop">
+			<div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				<p class="card-text"><a href="https://www.linkedin.com/in/jamie-amparan/" target="_blank">LinkedIn</a>
+					or
+					<a href="https://github.com/jleespooks/" target="_blank">Github</a>"</p>
+			</div>
+			<div class="card-footer">
+				<small class="text-muted">Last updated 3 mins ago</small>
+			</div>
+		</div>
+
+		<div class="card">
+			<img class="img-fluid my-3 md-rounded-circle" src="img/jesseprop.png" class="card-img-top" alt="prop">
+			<div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+			</div>
+			<div class="card-footer">
+				<small class="text-muted">Last updated 3 mins ago</small>
+			</div>
+		</div>
+	</div>
+
+</head>
--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -4,6 +4,7 @@
 	<head>
 		<!--Navbar beginning -->
 		<nav class="navbar navbar-dark bg-dark fluid d-flex justify-content-end text-monospace">
+
 			<form class="form-inline">
 				<button class="btn btn-outline-warning" type="button">Sign Up</button>
 				<button class="btn btn-outline-warning" type="button">Sign In</button>
