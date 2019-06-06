8127f8c6bb007674ecf97317eeac63e26955e07d
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-06-06 09:21:45 -0700

    Added files in src directory.

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import {Route, BrowserRouter, Switch} from "react-router-dom";
+import {Home} from "./pages/home/Home.js";
 
 
-const App = () => ( <h1 className="text-info">hello world</h1> );
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const Routing = () => (
+	<>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path = "/" component = {Home}/>
+			</Switch>
+		</BrowserRouter>
+	</>
+);
+
+ReactDOM.render(Routing(), document.querySelector('#root'));
---
+++ app/src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const HomeComponent = () => {
+	return (
+		<main className="container">
+			<h1>Hello</h1>
+		</main>
+	)
+};
+
+export const Home = (HomeComponent);
