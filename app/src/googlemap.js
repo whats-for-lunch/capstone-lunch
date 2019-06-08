c20783650575cbf659d7ca1402a0a9a0913eefe5
Jesse Silva <thebestjesse76@gmail.com>
2019-06-08 13:56:48 -0600

    tried to get map working

--- app/src/googlemap.js
+++ app/src/googlemap.js
@@ -1,17 +1,23 @@
-import { withScriptjs, withGoogleMap, GoogleMap Marker } from "react-google-maps"
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const MyMapComponent = (props) =>
-    <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    >
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-    </GoogleMap>
+export class MapContainer extends Component {
+    render() {
+        return (
+            <Map google={this.props.google} zoom={14}>
 
-    <MyMapComponent isMarkerShown />// Map with a Marker
-<MyMapComponent isMarkerShown={false} />// Just only Map
-googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-loadingElement={<div style={{ height: `100%` }} />}
-containerElement={<div style={{ height: `400px` }} />}
-mapElement={<div style={{ height: `100%` }} />}
-/>
+                <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
+            </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: (AIzaSyD06q5rM_DroPEG3scOlB3w-DVf_mv37ZI)
+})(MapContainer)
