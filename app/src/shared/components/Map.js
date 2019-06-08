0bfba7baf980e40ddd3832c0d1e849cf55971f27
Jamie Amparan <jleespooks@gmail.com>
2019-06-08 11:33:38 -0600

    Added map, recommended, favorite, and background to react.

---
+++ app/src/shared/components/Map.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export const Map = () => (
+	<body>
+
+
+		<div class="container">
+			<div class="container justify-content-center bg-dark">
+				<div class="row justify-content-center">
+	<span class="border border-warning">
+	<img src="img/yelpmap.jpg.png" height="500px" width="600px"/>
+	</span>
+				</div>
+			</div>
+		</div>
+
+	</body>
+)
