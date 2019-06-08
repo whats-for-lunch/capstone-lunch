0621fe961052ee38ff6dc19c4f43d5325845c493
Jamie Amparan <jleespooks@gmail.com>
2019-06-08 10:19:35 -0600

    added navbar, footer, about us using react

---
+++ app/src/shared/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export const Footer = () => (
+	<footer>
+
+		{/*<!-- Sticky Footer -->*/}
+			<div class="container-fluid py-5">
+				<div class="sticky-footer text-center bg-dark pt-2 pb-0 m-0 fixed-bottom">
+					<div class="content-center">
+						<a href="https://github.com/whats-for-lunch" class="github">
+							<i class="fab fa-github fa-2x"></i>
+						</a>
+						<a href="https://www.yelp.com/" class="ml-2 yelp">
+							<i class="fab fa-yelp fa-2x"></i>
+						</a>
+						<br/>
+					</div>
+				</div>
+			</div>
+		</footer>
+)
