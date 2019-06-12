92094b7ff5b4674028600cc67f7d2df0be4daa9d
Jesse Silva <thebestjesse76@gmail.com>
2019-06-12 14:41:43 -0600

    finished touches on favorite page now need to link home bar on nav

--- app/src/pages/Favorite.js
+++ app/src/pages/Favorite.js
@@ -12,62 +12,67 @@ export const Favorite = () => (
 		</div>
 
 
-		<section id="card decks">
-			<div className="container-fluid bg-dark text-dark">
-				<div className="container my-8">
-					<div className="card-deck">
-						<div className="card bg-warning">
-							<img className="card-img-top"
-								  src="https://s3-media2.fl.yelpcdn.com/bphoto/UEnfWw0AdsVhMzmvLswgzg/o.jpg"
-								  alt="Card image cap" className="rounded"/>
-								<div className="card-body">
-									<h5 className="card-title">Artichoke Cafe</h5>
-									<p className="card-text">American (New), Cafes</p>
-									<p><strong>424 Central Ave SE
-										Albuquerque, NM 87102</strong></p>
-									<i className="far fa-window-close"></i>
-								</div>
+		<div className="card-deck bg-dark">
+			<div className="card bg-warning p-1">
+				<img className="img-fluid rounded " src="https://s3-media2.fl.yelpcdn.com/bphoto/UDHrDaDc96XzdwbDIEP3gQ/o.jpg" alt="prop"/>
+				<div className="card-body">
+					<h5 className="card-title">Artichoke Cafe</h5>
+					<p className="card-text">424 Central Ave SE
+						Albuquerque, NM 87102
 
-						</div>
-						<div className="card bg-warning">
-							<img className="card-img-top"
-								  src="https://s3-media3.fl.yelpcdn.com/bphoto/-odb7y2_GxASimUqXtNn4Q/o.jpg"
-								  alt="Card image cap" className="rounded"/>
-								<div className="card-body">
-									<h5 className="card-title">Cocina Azul</h5>
-									<p className="card-text">New Mexican Cuisine </p>
-									<p><strong>1134 Mountain Rd NW
-										Albuquerque, NM 87102</strong></p>
-								</div>
-						</div>
-						<div className="card bg-warning">
-							<img className="card-img-top"
-								  src="https://s3-media3.fl.yelpcdn.com/bphoto/-odb7y2_GxASimUqXtNn4Q/o.jpg"
-								  alt="Card image cap" className="rounded"/>
-								<div className="card-body">
-									<h5 className="card-title">The Grove Cafe & Market</h5>
-									<p className="card-text">Coffee & Tea, American (New), Cafes </p>
-									<p><strong>600 Central Ave SE
-										Ste A
-										Albuquerque, NM 87102</strong></p>
-								</div>
-						</div>
-						<div className="card bg-warning">
-							<img className="card-img-top"
-								  src="https://s3-media2.fl.yelpcdn.com/bphoto/UEnfWw0AdsVhMzmvLswgzg/o.jpg"
-								  alt="Card image cap" className="rounded"/>
-								<div className="card-body">
-									<h5 className="card-title">Sophia’s</h5>
-									<p className="card-text">American (New), Breakfast & Brunch, Latin American</p>
-									<p><strong> Park Ave SW
-										Ste 102
-										Albuquerque, NM 87102</strong></p>
-								</div>
-						</div>
-					</div>
+					</p>
+				</div>
+				<div className="card-footer">
+					<a href="https://www.yelp.com/biz/artichoke-cafe-albuquerque-3"
+					   target="_blank">Artichoke Cafe</a>
 				</div>
 			</div>
-		</section>
+
+			<div className="card bg-warning p-1">
+				<img className="img-fluid rounded " src="https://s3-media4.fl.yelpcdn.com/bphoto/xaPRwhSswSxYpCnSwnwyGQ/o.jpg"  alt="prop"/>
+				<div className="card-body">
+					<h5 className="card-title">Lulu’s Kitchen</h5>
+					<p className="card-text">315 Gold Ave SW
+						Albuquerque, NM 87102
+
+					</p>
+				</div>
+				<div className="card-footer">
+					<a href="https://www.yelp.com/biz/lulus-kitchen-albuquerque-2"
+					   target="_blank">Lulu’s Kitchen</a>
+				</div>
+			</div>
+
+			<div className="card bg-warning p-1">
+				<img className="img-fluid  rounded" src="https://s3-media3.fl.yelpcdn.com/bphoto/8scPGxPpkztjo_u8ChnLXw/o.jpg" alt="prop"/>
+				<div className="card-body">
+					<h5 className="card-title">MAS - Tapas Y Vino</h5>
+					<p className="card-text">125 2nd St NW
+						Andaluz Hotel
+						Albuquerque, NM 87102
+
+					</p>
+				</div>
+				<div className="card-footer">
+					<a href="https://www.yelp.com/biz/mas-tapas-y-vino-albuquerque"
+					   target="_blank">MAS - Tapas Y Vino</a>
+				</div>
+			</div>
+
+			<div className="card bg-warning p-1">
+				<img className="img-fluid  rounded" src="https://s3-media1.fl.yelpcdn.com/bphoto/7MQ9uetuHmZTniMWqx7R3A/o.jpg" alt="prop"/>
+				<div className="card-body">
+					<h5 className="card-title">Tucanos Brazilian Grill</h5>
+					<p className="card-text">110 Central Ave SW
+						Albuquerque, NM 87102
+					</p>
+				</div>
+				<div className="card-footer">
+					<a href="https://www.yelp.com/biz/tucanos-brazilian-grill-albuquerque"
+					   target="_blank">Tucanos Brazilian Grill</a>
+				</div>
+			</div>
+		</div>
 
 	</body>
 	)
