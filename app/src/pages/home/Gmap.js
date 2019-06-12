ad140b8b413ef701ec3464a48e33af3fe291818b
Jesse Silva <thebestjesse76@gmail.com>
2019-06-12 15:57:20 -0600

    trying to map working with data from database

--- app/src/pages/home/Gmap.js
+++ app/src/pages/home/Gmap.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {GoogleMap, Marker, withGoogleMap, withScriptjs} from "react-google-maps";
+import {Modal} from "react-bootstrap";
+
 
 
 export const GMap = withScriptjs(withGoogleMap(({restaurants}) => {
-	(console.log(restaurants));
+
+	const [show, setShow] = useState(false);
+	const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+
+	const open = () => {
+		setShow(true);
+	};
+
+	const close = () => {
+		setShow(false);
+
+	};
 	return (
+		<>
+
+			{
+				(selectedRestaurant) && (restaurants)
+					<Modal show={show} onHide={close}>
+						<Modal.Header closeButton>
+							<Modal.Title>Modal heading</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>{}
+						</Modal.Body>
+					</Modal>
+				)
+			}
 			<GoogleMap
 				defaultZoom={14}
 				defaultCenter={{lat: 35.0856197, lng: -106.64924}}
@@ -14,9 +40,14 @@ export const GMap = withScriptjs(withGoogleMap(({restaurants}) => {
 				<Marker
 					position={{lat:restaurant.restaurantLat, lng:restaurant.restaurantLng}}
 					defaultPosition={{lat:35.0859, lng:106.6499}}
+					onClick={() => {
+						open();
+						setSelectedRestaurant(restaurant)
+					}}
 				/>
 			))}
 			</GoogleMap>
+		</>
 		)
 	}
 ));
